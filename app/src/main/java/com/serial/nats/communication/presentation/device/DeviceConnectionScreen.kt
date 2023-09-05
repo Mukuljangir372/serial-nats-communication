f4f045d15e4e0c90f24647d6c3603a7e6f4518f2
Mukul Jangir <mukul.jangir372@gmail.com>
2023-09-05 19:06:35 +0530

    feat[DeviceConnection]: minor fix

--- app/src/main/java/com/serial/nats/communication/presentation/device/DeviceConnectionScreen.kt
+++ app/src/main/java/com/serial/nats/communication/presentation/device/DeviceConnectionScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -18,6 +19,11 @@ import com.serial.nats.communication.presentation.device.resource.DeviceConnecti
 fun DeviceConnectionScreen() {
     val viewModel = hiltViewModel<DeviceConnectionViewModel>()
     val state by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(Unit) {
+        viewModel.load()
+    }
+
     DeviceConnectionScreenContent(state = state)
 }
 
