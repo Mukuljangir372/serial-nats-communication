4491ed45f588a0bd8d367418849a618dddd5a5a4
Mukul Jangir <mukul.jangir372@gmail.com>
2023-09-05 17:35:59 +0530

    chore[Gradle]: deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,9 +55,9 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
 
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -65,8 +65,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    implementation("androidx.navigation:navigation-compose:2.7.1")
+
+    implementation("com.google.dagger:hilt-android:2.47")
+    kapt("com.google.dagger:hilt-android-compiler:2.47")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
 
