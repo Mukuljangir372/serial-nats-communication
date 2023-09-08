c5cef873e89b5db8f0154a5cbf91bfed00c8c91b
Mukul Jangir <mukul.jangir372@gmail.com>
2023-09-08 13:51:58 +0530

    fix[DeviceConnection]: cache ports

--- app/src/main/java/com/serial/nats/communication/core/device/manager/DeviceManagerImpl.kt
+++ app/src/main/java/com/serial/nats/communication/core/device/manager/DeviceManagerImpl.kt
@@ -102,8 +102,9 @@ class DeviceManagerImpl(
                 val driver = getDeviceDriver(device)
                 driver.ports.map { port ->
                     val cachePort = cacheSerialPorts["${driver.device.deviceId}+${port.portNumber}"]
-                    if (cachePort == null) cacheSerialPorts["${driver.device.deviceId}+${port.portNumber}"] =
-                        port
+                    if (cachePort == null) {
+                        cacheSerialPorts["${driver.device.deviceId}+${port.portNumber}"] = port
+                    }
                     convertDriverToNativeDevice(
                         driver = driver,
                         port = cachePort ?: port
